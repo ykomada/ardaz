184d2be7c8177b51b6f2beae3aa1a30f9a7941cc
Yoshitake Komada <yoshmeister@Yoshitakes-MacBook-Air.local>
2015-02-20 15:46:12 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ardaz::Application.config.secret_token = 'b5c221bfccdd842df2c4d81778ca702a4cf1cf943c15ef140284347fa21c8d2860a48f104629e39f96a9af48e09c258e35e3c56f65a32ad8a16bf2aef1d45a6a'
